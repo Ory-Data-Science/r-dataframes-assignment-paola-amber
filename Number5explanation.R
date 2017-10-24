350dbe0e12297576345cdaa58ed2bbae758efda3
avakassi <amber.vakassi@stlcop.edu>
2017-10-24 18:26:23 -0500

    Explanation

---
+++ Number5explanation.R
@@ -0,0 +1,8 @@
+
+After inputting the incorrect code, we were able to analyze which parts of it are going to give the correct ouput
+and which ones will not. The R script tool allows us to easily view the codes and input each one individually,
+until we are ready to "run" them. The R script analyzes each code and automatically inputs an error warning
+in the case of a code which is incorrect. In the case of a data file that is thousands of lines long, the R script
+would allow for it to be easily detectable as to which code is incorrect and where the error is exactly. 
+It would be beneficial to "check" if the codes are giving the correct outputs by sorting through the data and 
+examining whether a small portion is correct in terms of the larger scale. 
